ec411d07e170dd2888dd6bb3bc1ab1bffd77a7c7
Datong Sun <dndx@idndx.com>
2017-08-31 17:45:44 -0700

    style: removed excessive whitespace control. #0bb4ea5

--- src/ngx_stream_lua_control.c
+++ src/ngx_stream_lua_control.c
@@ -81,10 +81,12 @@ ngx_stream_lua_ngx_exit(lua_State *L)
 
     ngx_log_debug1(NGX_LOG_DEBUG_STREAM, r->connection->log, 0,
                    "lua exit with code %i", ctx->exit_code);
+
     if (ctx->context & NGX_STREAM_LUA_CONTEXT_BALANCER)
     {
         return 0;
     }
+
     dd("calling yield");
     return lua_yield(L, 0);
 }
