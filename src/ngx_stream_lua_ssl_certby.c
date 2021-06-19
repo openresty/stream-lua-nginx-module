7930446d0dd31ef11ceebcea9481de7c99d1aaaa
罗泽轩 <spacewanderlzx@gmail.com>
2021-06-19 19:17:21 +0800

    feature: allow to be compiled with LibreSSL 3.0+ (#249)

--- src/ngx_stream_lua_ssl_certby.c
+++ src/ngx_stream_lua_ssl_certby.c
@@ -1369,6 +1369,13 @@ int
 ngx_stream_lua_ffi_ssl_verify_client(ngx_stream_lua_request_t *r,
     void *ca_certs, int depth, char **err)
 {
+#ifdef LIBRESSL_VERSION_NUMBER
+
+    *err = "LibreSSL not supported";
+    return NGX_ERROR;
+
+#else
+
     ngx_stream_lua_ctx_t        *ctx;
     ngx_ssl_conn_t              *ssl_conn;
     ngx_stream_ssl_conf_t       *sscf;
@@ -1487,6 +1494,7 @@ failed:
     X509_STORE_free(ca_store);
 
     return NGX_ERROR;
+#endif
 }
 
 
