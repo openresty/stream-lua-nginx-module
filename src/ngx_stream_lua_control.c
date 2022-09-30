12198646f1fd3b8e08c1cf7fffddc1ae920b6c5e
spacewander <spacewanderlzx@gmail.com>
2022-09-30 11:26:58 +0800

    fix context
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- src/ngx_stream_lua_control.c
+++ src/ngx_stream_lua_control.c
@@ -113,7 +113,12 @@ ngx_stream_lua_ffi_exit(ngx_stream_lua_request_t *r, int status, u_char *err,
     }
 
 
-    if (ngx_stream_lua_ffi_check_context(ctx, NGX_STREAM_LUA_CONTEXT_YIELDABLE,
+    if (ngx_stream_lua_ffi_check_context(ctx, NGX_STREAM_LUA_CONTEXT_CONTENT
+        | NGX_STREAM_LUA_CONTEXT_TIMER
+        | NGX_STREAM_LUA_CONTEXT_BALANCER
+        | NGX_STREAM_LUA_CONTEXT_SSL_CLIENT_HELLO
+        | NGX_STREAM_LUA_CONTEXT_SSL_CERT
+        | NGX_STREAM_LUA_CONTEXT_PREREAD,
         err, errlen) != NGX_OK)
     {
         return NGX_ERROR;
