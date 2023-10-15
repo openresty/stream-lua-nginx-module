openresty/stream-lua-nginx-module

commit 08841a243f1a8bf4204a3643be96489361a46dd7

feature: support pcre2

src/ngx_stream_lua_common.h
@@ -47,6 +47,10 @@
 
 #endif
 
+#if (NGX_PCRE2)
+#   define LUA_HAVE_PCRE_JIT 1
+#endif
+
 
 #if !defined(nginx_version) || nginx_version < 1013006
 #error at least nginx 1.13.6 is required but found an older version
@@ -189,15 +193,16 @@ struct ngx_stream_lua_main_conf_s {
 
     ngx_connection_t    *watcher;  /* for watching the process exit event */
 
-#if (NGX_PCRE)
+#if (NGX_PCRE || NGX_PCRE2)
     ngx_int_t            regex_cache_entries;
     ngx_int_t            regex_cache_max_entries;
     ngx_int_t            regex_match_limit;
 
-#if (LUA_HAVE_PCRE_JIT)
+#if (NGX_PCRE2)
+    pcre2_jit_stack     *jit_stack;
+#elif (LUA_HAVE_PCRE_JIT)
     pcre_jit_stack      *jit_stack;
-#endif
-
+#   endif
 #endif
 
     ngx_array_t         *shm_zones;  /* of ngx_shm_zone_t* */

.travis.yml
@@ -29,9 +29,13 @@ env:
     - LUAJIT_INC=$LUAJIT_PREFIX/include/luajit-2.1
     - LUA_INCLUDE_DIR=$LUAJIT_INC
     - PCRE_VER=8.45
+    - PCRE2_VER=10.37
     - PCRE_PREFIX=/opt/pcre
+    - PCRE2_PREFIX=/opt/pcre2
     - PCRE_LIB=$PCRE_PREFIX/lib
+    - PCRE2_LIB=$PCRE2_PREFIX/lib
     - PCRE_INC=$PCRE_PREFIX/include
+    - PCRE2_INC=$PCRE2_PREFIX/include
     - OPENSSL_PREFIX=/opt/ssl
     - OPENSSL_LIB=$OPENSSL_PREFIX/lib
     - OPENSSL_INC=$OPENSSL_PREFIX/include
@@ -42,7 +46,7 @@ env:
     - NGINX_VERSION=1.21.4 OPENSSL_VER=1.1.0l
     - NGINX_VERSION=1.21.4 OPENSSL_VER=1.1.1s
     - NGINX_VERSION=1.25.1 OPENSSL_VER=1.1.0l
-    - NGINX_VERSION=1.25.1 OPENSSL_VER=1.1.1s
+    - NGINX_VERSION=1.25.1 OPENSSL_VER=1.1.1s USE_PCRE2=Y
 
 services:
  - memcache
@@ -51,7 +55,8 @@ services:
 install:
   - sudo apt update
   - sudo apt install --only-upgrade ca-certificates
-  - if [ ! -f download-cache/pcre-$PCRE_VER.tar.gz ]; then wget -P download-cache/ https://downloads.sourceforge.net/project/pcre/pcre/${PCRE_VER}/pcre-${PCRE_VER}.tar.gz; fi
+  - if [ "$USE_PCRE2" != "Y" ] && [ ! -f download-cache/pcre-$PCRE_VER.tar.gz ]; then wget -P download-cache/ https://downloads.sourceforge.net/project/pcre/pcre/${PCRE_VER}/pcre-${PCRE_VER}.tar.gz; fi
+  - if [ "$USE_PCRE2" = "Y" ] && [ ! -f download-cache/pcre2-$PCRE2_VER.tar.gz ]; then wget -P download-cache https://downloads.sourceforge.net/project/pcre/pcre2/${PCRE2_VER}/pcre2-${PCRE2_VER}.tar.gz; fi
   - if [ ! -f download-cache/openssl-$OPENSSL_VER.tar.gz ]; then wget -P download-cache https://www.openssl.org/source/openssl-$OPENSSL_VER.tar.gz || wget -P download-cache https://www.openssl.org/source/old/${OPENSSL_VER//[a-z]/}/openssl-$OPENSSL_VER.tar.gz; fi
   - git clone https://github.com/openresty/openresty-devel-utils.git
   - git clone https://github.com/openresty/lua-cjson.git
@@ -75,12 +80,8 @@ script:
   - sudo ip addr add 10.254.254.1/24 dev lo
   - sudo ip addr add 10.254.254.2/24 dev lo
   - sudo ip route add prohibit 0.0.0.1/32
-  - tar zxf download-cache/pcre-$PCRE_VER.tar.gz
-  - cd pcre-$PCRE_VER/
-  - ./configure --prefix=$PCRE_PREFIX --enable-jit --enable-utf --enable-unicode-properties > build.log 2>&1 || (cat build.log && exit 1)
-  - make -j$JOBS > build.log 2>&1 || (cat build.log && exit 1)
-  - sudo PATH=$PATH make install > build.log 2>&1 || (cat build.log && exit 1)
-  - cd ..
+  - if [ "$USE_PCRE2" != "Y" ]; then tar zxf download-cache/pcre-$PCRE_VER.tar.gz; cd pcre-$PCRE_VER/; ./configure --prefix=$PCRE_PREFIX --enable-jit --enable-utf --enable-unicode-properties > build.log 2>&1 || (cat build.log && exit 1); make -j$JOBS > build.log 2>&1 || (cat build.log && exit 1); sudo PATH=$PATH make install > build.log 2>&1 || (cat build.log && exit 1); cd ..; fi
+  - if [ "$USE_PCRE2" = "Y" ]; then tar zxf download-cache/pcre2-$PCRE2_VER.tar.gz; cd pcre2-$PCRE2_VER/; ./configure --prefix=$PCRE2_PREFIX --enable-jit --enable-utf > build.log 2>&1 || (cat build.log && exit 1); make -j$JOBS > build.log 2>&1 || (cat build.log && exit 1); sudo PATH=$PATH make install > build.log 2>&1 || (cat build.log && exit 1); cd ..; fi
   - cd luajit2
   - make -j$JOBS CCDEBUG=-g Q= PREFIX=$LUAJIT_PREFIX CC=$CC XCFLAGS='-DLUA_USE_APICHECK -DLUA_USE_ASSERT' > build.log 2>&1 || (cat build.log && exit 1)
   - sudo make install PREFIX=$LUAJIT_PREFIX > build.log 2>&1 || (cat build.log && exit 1)
@@ -103,4 +104,4 @@ script:
   - export TEST_NGINX_RESOLVER=8.8.4.4
   - dig +short @$TEST_NGINX_RESOLVER openresty.org || exit 0
   - dig +short @$TEST_NGINX_RESOLVER agentzh.org || exit 0
-  - prove -I. -r t
+  - prove -I. -Itest-nginx/lib -r t

config
@@ -405,7 +405,7 @@ fi
 
 # ----------------------------------------
 
-if [ $USE_PCRE = YES -o $PCRE != NONE ] && [ $PCRE != NO -a $PCRE != YES ]; then
+if [ $USE_PCRE = YES -o $PCRE != NONE ] && [ $PCRE != NO -a $PCRE != YES ] && [ $PCRE2 != YES ]; then
     # force pcre_version symbol to be required when PCRE is statically linked
     case "$NGX_PLATFORM" in
         Darwin:*)

src/ngx_stream_lua_module.c
@@ -56,6 +56,9 @@ static char *ngx_stream_lua_ssl_conf_command_check(ngx_conf_t *cf, void *post,
 #endif
 static char *ngx_stream_lua_malloc_trim(ngx_conf_t *cf, ngx_command_t *cmd,
     void *conf);
+#if (NGX_PCRE2)
+extern void ngx_stream_lua_regex_cleanup(void *data);
+#endif
 
 
 static ngx_conf_post_t  ngx_stream_lua_lowat_post =
@@ -577,7 +580,16 @@ ngx_stream_lua_init(ngx_conf_t *cf)
     cln->data = lmcf;
     cln->handler = ngx_stream_lua_sema_mm_cleanup;
 
+#if (NGX_PCRE2)
+    /* add the cleanup of pcre2 regex */
+    cln = ngx_pool_cleanup_add(cf->pool, 0);
+    if (cln == NULL) {
+        return NGX_ERROR;
+    }
 
+    cln->data = lmcf;
+    cln->handler = ngx_stream_lua_regex_cleanup;
+#endif
 
     if (lmcf->lua == NULL) {
         dd("initializing lua vm");

src/ngx_stream_lua_pcrefix.c
@@ -22,18 +22,63 @@
 #include "ngx_stream_lua_pcrefix.h"
 #include "stdio.h"
 
-#if (NGX_PCRE)
+#if (NGX_PCRE || NGX_PCRE2)
 
 static ngx_pool_t *ngx_stream_lua_pcre_pool = NULL;
 
+#if (NGX_PCRE2)
+static ngx_uint_t  ngx_regex_direct_alloc;
+#else
 static void *(*old_pcre_malloc)(size_t);
 static void (*old_pcre_free)(void *ptr);
+#endif
 
 
 /* XXX: work-around to nginx regex subsystem, must init a memory pool
  * to use PCRE functions. As PCRE still has memory-leaking problems,
  * and nginx overwrote pcre_malloc/free hooks with its own static
  * functions, so nobody else can reuse nginx regex subsystem... */
+#if (NGX_PCRE2)
+
+void *
+ngx_stream_lua_pcre_malloc(size_t size, void *data)
+{
+    dd("lua pcre pool is %p", ngx_stream_lua_pcre_pool);
+
+    if (ngx_stream_lua_pcre_pool) {
+        return ngx_palloc(ngx_stream_lua_pcre_pool, size);
+    }
+
+    if (ngx_regex_direct_alloc) {
+        return ngx_alloc(size, ngx_cycle->log);
+    }
+
+    fprintf(stderr, "error: lua pcre malloc failed due to empty pcre pool");
+
+    return NULL;
+}
+
+
+void
+ngx_stream_lua_pcre_free(void *ptr, void *data)
+{
+    dd("lua pcre pool is %p", ngx_stream_lua_pcre_pool);
+
+    if (ngx_stream_lua_pcre_pool) {
+        ngx_pfree(ngx_stream_lua_pcre_pool, ptr);
+        return;
+    }
+
+    if (ngx_regex_direct_alloc) {
+        ngx_free(ptr);
+        return;
+    }
+
+    fprintf(stderr, "error: lua pcre free failed due to empty pcre pool");
+}
+
+#else
+
 static void *
 ngx_stream_lua_pcre_malloc(size_t size)
 {
@@ -62,6 +107,41 @@ ngx_stream_lua_pcre_free(void *ptr)
     fprintf(stderr, "error: lua pcre free failed due to empty pcre pool");
 }
 
+#endif
+
+
+#if (NGX_PCRE2)
+
+ngx_pool_t *
+ngx_stream_lua_pcre_malloc_init(ngx_pool_t *pool)
+{
+    ngx_pool_t          *old_pool;
+
+    dd("lua pcre pool was %p", ngx_stream_lua_pcre_pool);
+
+    ngx_regex_direct_alloc = (pool == NULL) ? 1 : 0;
+
+    old_pool = ngx_stream_lua_pcre_pool;
+    ngx_stream_lua_pcre_pool = pool;
+
+    dd("lua pcre pool is %p", ngx_stream_lua_pcre_pool);
+
+    return old_pool;
+}
+
+
+void
+ngx_stream_lua_pcre_malloc_done(ngx_pool_t *old_pool)
+{
+    dd("lua pcre pool was %p", ngx_stream_lua_pcre_pool);
+
+    ngx_stream_lua_pcre_pool = old_pool;
+    ngx_regex_direct_alloc = 0;
+
+    dd("lua pcre pool is %p", ngx_stream_lua_pcre_pool);
+}
+
+#else
 
 ngx_pool_t *
 ngx_stream_lua_pcre_malloc_init(ngx_pool_t *pool)
@@ -109,6 +189,7 @@ ngx_stream_lua_pcre_malloc_done(ngx_pool_t *old_pool)
     }
 }
 
-#endif /* NGX_PCRE */
+#endif
+#endif /* NGX_PCRE || NGX_PCRE2 */
 
 /* vi:set ft=c ts=4 sw=4 et fdm=marker: */

src/ngx_stream_lua_pcrefix.h
@@ -20,9 +20,14 @@
 #include "ngx_stream_lua_common.h"
 
 
-#if (NGX_PCRE)
+#if (NGX_PCRE || NGX_PCRE2)
 ngx_pool_t *ngx_stream_lua_pcre_malloc_init(ngx_pool_t *pool);
 void ngx_stream_lua_pcre_malloc_done(ngx_pool_t *old_pool);
+
+#if NGX_PCRE2
+void *ngx_stream_lua_pcre_malloc(size_t size, void *data);
+void ngx_stream_lua_pcre_free(void *ptr, void *data);
+#endif
 #endif
 
 

t/034-match.t
@@ -287,8 +287,11 @@ he
             end
         end
     }
---- stream_response
-error: pcre_compile() failed: missing ) in "(abc"
+--- stream_response eval
+$Test::Nginx::Util::PcreVersion == 2 ?
+"error: pcre2_compile() failed: missing closing parenthesis in \"(abc\"\n"
+:
+"error: pcre_compile() failed: missing ) in \"(abc\"\n"
 --- no_error_log
 [error]
 
@@ -505,8 +508,11 @@ regex: (?:>[\w\s]*</?\w{2,}>)
             end
         end
     }
---- stream_response
-error: pcre_compile() failed: missing ) in "([0-9]+"
+--- stream_response eval
+$Test::Nginx::Util::PcreVersion == 2 ?
+"error: pcre2_compile\(\) failed: missing closing parenthesis in \"\([0-9]+\"\n"
+:
+"error: pcre_compile\(\) failed: missing \) in \"\([0-9]+\"\n"
 
 --- no_error_log
 [error]
@@ -745,8 +751,11 @@ nil
             ngx.say("not matched")
         end
     }
---- stream_response_like chop
-^error: pcre_exec\(\) failed: -10$
+--- stream_response eval
+$Test::Nginx::Util::PcreVersion == 2 ?
+"error: pcre_exec\(\) failed: -4\n"
+:
+"error: pcre_exec\(\) failed: -10\n"
 
 --- no_error_log
 [error]
@@ -844,8 +853,14 @@ if not res then
     return
 end
 
---- stream_response
-error: pcre_exec() failed: -8
+--- stream_response eval
+# lua_regex_match_limit uses pcre_extra->match_limit in the PCRE,
+# but PCRE2 replaces this with pcre2_set_match_limit interface,
+# which has different effects.
+$Test::Nginx::Util::PcreVersion == 2 ?
+"failed to match\n"
+:
+"error: pcre_exec() failed: -8\n"
 
 
 

commit f1d5c46c55a39d895690a9614d1755259c93de3b

bugfix: don't include pcre.h with PCRE2 used
pcre.h is a PCRE header and is not exposed by PCRE2 library causing
compilation error as the header is not found.

Don't include pcre.h if nginx is compiled with PCRE2 support enabled.

Fixes: 08841a243f1a ("feature: support pcre2")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

src/ngx_stream_lua_common.h
@@ -35,7 +35,7 @@
 #include "ngx_stream_lua_request.h"
 
 
-#if (NGX_PCRE)
+#if defined(NGX_PCRE) && !defined(NGX_PCRE2)
 
 #include <pcre.h>
 