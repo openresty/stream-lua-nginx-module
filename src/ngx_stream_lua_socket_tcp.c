56b858dce1b0eb0b401dcb30b868c8d079471ce7
Sergey A. Osokin <osa@FreeBSD.org>
2026-01-16 23:05:40 -0500

    optimize: add compatibility for freenginx.
    Commit freenginx/nginx@3329aa9 updated the ngx_http_request_t
    structure with the start_time; the start_sec and start_msec
    were removed.
    
    Also, some additional SSL-related functionality has been added
    to the the nginx, however, a corresponding one for the freenginx
    is missing.

--- src/ngx_stream_lua_socket_tcp.c
+++ src/ngx_stream_lua_socket_tcp.c
@@ -2261,7 +2261,9 @@ ngx_stream_lua_socket_tcp_serversslhandshake(lua_State *L)
     ngx_stream_lua_ctx_t                        *ctx;
     ngx_stream_lua_co_ctx_t                     *coctx;
     ngx_stream_lua_socket_tcp_upstream_t        *u;
+#if !defined (freenginx)
     ngx_stream_ssl_srv_conf_t                   *sscf;
+#endif
 
     /* Lua function arguments: self */
 
@@ -2317,6 +2319,7 @@ ngx_stream_lua_socket_tcp_serversslhandshake(lua_State *L)
         return 1;
     }
 
+#if !defined (freenginx)
     sscf = ngx_stream_get_module_srv_conf(r->session, ngx_stream_ssl_module);
 
     if (sscf == NULL || sscf->ssl.ctx == NULL) {
@@ -2330,6 +2333,7 @@ ngx_stream_lua_socket_tcp_serversslhandshake(lua_State *L)
         lua_pushliteral(L, "failed to create ssl connection");
         return 2;
     }
+#endif
 
     ctx = ngx_stream_lua_get_module_ctx(r, ngx_stream_lua_module);
     if (ctx == NULL) {
--- src/ngx_stream_lua_time.c
+++ src/ngx_stream_lua_time.c
@@ -36,7 +36,21 @@ ngx_stream_lua_ffi_now(void)
 double
 ngx_stream_lua_ffi_req_start_time(ngx_stream_lua_request_t *r)
 {
+#if defined freenginx
+    ngx_time_t              *tp;
+
+    tp = ngx_timeofday();
+    tp->sec -= (ngx_current_msec - r->session->start_time) / 1000;
+    tp->msec -= (ngx_current_msec - r->session->start_time) % 1000;
+    if (tp->msec > NGX_MAX_INT_T_VALUE) {
+        tp->msec += 1000;
+        tp->sec -= 1;
+    }
+
+    return tp->sec + tp->msec / 1000.0;
+#else
     return r->session->start_sec + r->session->start_msec / 1000.0;
+#endif
 }
 
 
